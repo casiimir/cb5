8fb5271c98e022a8396220848e0ad8f5feba6fb2
casiimir <73138891+casiimir@users.noreply.github.com>
2022-11-17 12:43:05 +0100

    Lezione 17 novembre 2022

---
+++ 17-11-22/README.md
@@ -0,0 +1,11 @@
+### Esercitazione
+
+Sulla base del giorno, e di quanto detto a lezione, provare a risolvere il problema legato alla chiamata fetch. Ricordate di rispettare i seguenti punti:
+
+- la chiamata deve essere una (se via aiuta, cominciate a risolvere anche fancendolo con chiamate multiple all'inizio)
+- i prodotti renderizzati saranno le sole cards che presentano il testo, digitato nella input, incluso nel titolo del singolo progotto (`product.title` per ex.).
+
+### Avanzato
+
+Provare a completare aggiungendo una modale che presento la struttura per una chiamata di tipo `POST`.
+Con struttura si intende tutto lo stretto necessario a livello di codice, non è indispensabile che effettivamente scriva in remoto, ma il codice in potenza deve esserlo!
---
+++ 17-11-22/script.js
@@ -0,0 +1,48 @@
+import { qs, ce, GET } from "./utils.js";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com/photos";
+const cardList = qs(".cardList");
+const inputEl = qs(".search-input");
+let inputValue = "";
+let productsList = [];
+
+const createCardEl = (data, parent) => {
+  const { thumbnailUrl, title, url } = data;
+
+  // const elements = {
+  //   cardEl: ce("div"),
+  //   imgEl: ce("img"),
+  //   titleEl: ce("h4"),
+  //   urlEl: ce("p")
+  // }
+  const cardEl = ce("div");
+  const imgEl = ce("img");
+  const titleEl = ce("h4");
+  const urlEl = ce("span");
+
+  cardEl.className = "card";
+  imgEl.className = "card__img";
+  imgEl.setAttribute("src", thumbnailUrl);
+  imgEl.setAttribute("alt", title);
+  titleEl.textContent = title;
+  titleEl.className = "card__title";
+  urlEl.className = "card__text";
+  urlEl.textContent = url;
+
+  cardEl.append(imgEl, titleEl, urlEl);
+  parent.append(cardEl);
+};
+
+GET(BASE_URL).then((data) => {
+  productsList = data.filter((product) => product.id <= 10);
+  // const filteredByInput = justTenProd.filter((product) =>
+  //   product.title.includes(inputValue)
+  // );
+  // filteredByInput.map((product) => createCardEl(product, cardList));
+});
+
+inputEl.addEventListener("input", (e) => {
+  inputValue = "";
+  inputValue += e.target.value;
+  console.log(inputValue);
+});
